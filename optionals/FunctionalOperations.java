927288d5de3a0d530c91d812e5f03a8f66b34e62
Irshad Ahamad <irshu79054@gmail.com>
2026-02-26 15:10:21 +0530

    optionals

--- optionals/FunctionalOperations.java
+++ optionals/FunctionalOperations.java
@@ -1,5 +1,36 @@
 package optionals;
 
+import java.util.Optional;
+
 public class FunctionalOperations {
+	public static void main(String[] args) {
+		
+		// ifPresent()
+		// map()
+		// filter()
+		
+		Optional<String> name = Optional.of("JAVA");
+		Optional<String> nullOptional = Optional.ofNullable(null);
+		
+		//ifPresent()
+		nullOptional.ifPresent(value -> {
+			System.out.println(value);
+			System.out.println("Hello!!");
+		});
+		
+		  // map()
+        Optional<String> upperCase = name.map(String::toUpperCase);
+        System.out.println(upperCase.orElse("default"));
+        
+        //filter()
+        name.filter(n -> n.startsWith("J"))
+        .ifPresent(System.out::println);
+        
+        // Combined Operation
+        name.filter(n -> n.startsWith("F"))
+                .map(String::toUpperCase)
+                .ifPresent(System.out::println);
+	}
+	
 
 }
